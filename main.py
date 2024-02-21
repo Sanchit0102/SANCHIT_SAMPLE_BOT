2027f4f99d86849b2c0c6ac8737692673afa441e
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-02-21 17:22:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+#(©) 𝚂𝙰𝙽𝙲𝙷𝙸𝚃 ♛⛧
 from pyrogram import Client,filters
 from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton 
 
 
-API_ID = "24830912"
-API_HASH = "a1a1775593531b90850b8b82e3b14940"
-BOT_TOKEN = "6433293774:AAGRhgl7a9Oi9XhbZnKE_yzTqc8n1KsymZY"
+API_ID = "25833520"
+API_HASH = "7d012a6cbfabc2d0436d7a09d8362af7"
+BOT_TOKEN = "6362281925:AAGSTs25eytppfuN-AF-IMKCtxrvtxhXWJ8"
 
-TeluguZone = Client(
+Sanchit = Client(
     name="Samplebot",
     api_id=API_ID,
     api_hash=API_HASH,
@@ -14,33 +15,33 @@ TeluguZone = Client(
 )
 
 
-@TeluguZone.on_message(filters.command("start"))
+@Sanchit.on_message(filters.command("start"))
 async def start_cmd(cleint,message):
     await message.reply_photo(
-        photo="https://graph.org/file/3dc0d36c2bab91936a26b.jpg",
-        caption="This Is A Advanced @Daemon990 Bot") 
+        photo="https://telegra.ph/file/2ab88e1d0a62e75e4b2ff.jpg",
+        caption="This Is A Advanced Bot") 
 
-@TeluguZone.on_message(filters.command("help"))
+@Sanchit.on_message(filters.command("help"))
 async def help_cmd(cleint, message):
     await message.reply_text("hi contact my father DaEmoN")
 
-@TeluguZone.on_message(filters.command("about"))
+@Sanchit.on_message(filters.command("about"))
 async def about_cmd(cleint, message):
     await message.reply_text("</a>ᴍʏ ɴᴀᴍᴇ : Cinema Explorer Bot
 
  ‣ ᴍʏ ʙᴇꜱᴛ ꜰʀɪᴇɴᴅ: <a href='tg://settings'>ᴛʜɪs ᴘᴇʀsᴏɴ</a> 
 
- ‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='http://t.me/Daemon990'>DAEMON</a>")
+ ‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='http://t.me/THE_DS_OFFICIAL'>🤧</a>")
 
 
-@TeluguZone.on_message(filters.command("start") & filters.incoming)
+@Sanchit.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     [[
-    InlineKeyboardButton("JOIN MY UPDATES CHANNEL", url="https://t.me/TeluguZone0")
+    InlineKeyboardButton("JOIN MY UPDATES CHANNEL", url="https://t.me/THE_SILENT_TEAMS")
 ]]
     
 
 print("Bot was Started")
 
-TeluguZone.run()
+Sanchit.run()
   
